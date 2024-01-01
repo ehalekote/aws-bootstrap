f8cc2bcac6623b8cede787d323a86d9fcf378dfb
Eshan Halekote <ehalekote@Eshans-Laptop.lan>
2024-01-01 17:58:21 -0600

    logging for stop-service.sh

--- stop-service.sh
+++ stop-service.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
+echo "Current PATH: $PATH"
+echo "Current Shell: $SHELL"
 [ -d "/home/ec2-user/app/release" ] && cd /home/ec2-user/app/release && npm stop
