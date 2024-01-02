5f0764f5c93b02391125d15c38336649eb4d9881
Eshan Halekote <ehalekote@Eshans-Laptop.lan>
2024-01-02 13:38:41 -0600

    debugging and node modules

--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-echo "Current PATH: $PATH"
-echo "Current Shell: $SHELL"
+echo "stop-service.sh Current PATH: $PATH"
+echo "stop-service.sh Current Shell: $SHELL"
 [ -d "/home/ec2-user/app/release" ] && cd /home/ec2-user/app/release && npm stop
